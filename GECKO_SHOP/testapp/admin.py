0aacc8c2a032d26eec51223c5064378fe5e51147
Ben Guiden <ben.guiden2@mail.dcu.ie>
2021-03-05 15:46:55 +0000

    forms

--- GECKO_SHOP/testapp/admin.py
+++ GECKO_SHOP/testapp/admin.py
@@ -3,7 +3,8 @@ from django.contrib import admin
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Order)
-admin.site.register(Stock)
-admin.site.register(Customer)
+#admin.site.register(Stock)
+#admin.site.register(Customer)
 admin.site.register(Category)
+admin.site.register(CaUser)
 admin.site.register(Reviews)

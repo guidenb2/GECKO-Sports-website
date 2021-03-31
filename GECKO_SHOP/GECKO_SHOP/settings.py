55a2568b04ec2286c731a4e58b456db442c0c208
Harish-na <Harishns2001@gmail.com>
2021-03-31 14:12:26 +0100

    added method to fetch data from localhost 8000

--- GECKO_SHOP/GECKO_SHOP/settings.py
+++ GECKO_SHOP/GECKO_SHOP/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
+    'corsheaders',
     'testapp'
 ]
 
@@ -59,7 +60,15 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
+
+
+CORS_ALLOWED_ORIGINS = [
+ "http://localhost:3000", # node on port 3000
+ "http://127.0.0.1:3000" # node on port 3000
+]
+
 
 ROOT_URLCONF = 'GECKO_SHOP.urls'
 

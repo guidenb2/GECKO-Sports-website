a9928457be43539bb42487dcf65d1e502b9abe3f
Harish-na <Harishns2001@gmail.com>
2021-04-18 11:16:31 +0100

    Added Reviews Page to node app (Fetches from DJANGO)

--- GECKO_SHOP_NODE/routes/index.js
+++ GECKO_SHOP_NODE/routes/index.js
@@ -2,8 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res, next)
+{
   res.render('index', {title: "GECKO SHOP"});
+});
+
+
+/* GET home page. */
+router.get('/reviews', function(req, res, next)
+{
+  res.render('reviews', {title: "GECKO SHOP"});
 });
 
 module.exports = router;

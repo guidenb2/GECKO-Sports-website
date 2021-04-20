26c970faa90c5efca9c85af035f85eb74ed321b6
Ben Guiden <ben.guiden2@mail.dcu.ie>
2021-04-20 22:19:57 +0100

    Changed to single page application

--- GECKO_SHOP_NODE/routes/index.js
+++ GECKO_SHOP_NODE/routes/index.js
@@ -7,28 +7,6 @@ router.get('/', function(req, res, next)
   res.render('index', {title: "GECKO SHOP"});
 });
 
-
-/* GET reviews page. */
-router.get('/reviews', function(req, res, next)
-{
-  res.render('reviews', {title: "Reviews"});
-});
-
-
-/* GET products page. */
-router.get('/products', function(req, res, next)
-{
-  res.render('products', {title: "Products"});
-});
-
-
-/* GET about page. */
-router.get('/about', function(req, res, next)
-{
-  res.render('about', {title: "About"});
-});
-
-
 router.get('/login', function(req, res, next)
 {
   res.render('userlogin', {title: "Login"});

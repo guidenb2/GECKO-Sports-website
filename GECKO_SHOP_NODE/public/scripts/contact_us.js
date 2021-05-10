8fa8b966fce7f9f43092f1e1787ec87ec52042fa
Harish-na <Harishns2001@gmail.com>
2021-05-10 17:31:27 +0100

    moved scripts to contact_us.js

---
+++ GECKO_SHOP_NODE/public/scripts/contact_us.js
@@ -0,0 +1,4 @@
+let button2 = document.getElementById("contact_us_button")
+button2.addEventListener('click', function(){
+    alert("Thank you so much for reaching out to us!!\nWe'll be in touch soon!!")
+})
